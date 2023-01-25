e45e520379318e43a2086c1214e1a12a1579350d
Lucas Reis Oliveira Vargas Lomiento <lucaslomiento@icloud.com>
2023-01-25 19:28:07 -0300

    arrumando imports de alguns arquivos

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import RecipeInProgress from './pages/RecipeInProgress';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import Profile from './pages/Profile';
-import Login from './components/Login';
+import Login from './pages/Login';
 
 function App() {
   return (
