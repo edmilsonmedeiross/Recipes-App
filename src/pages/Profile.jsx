bc0c2a2fc95376393c0b9667a40249fcace7cf91
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-02-01 17:45:50 -0300

    adiciona email, botoes de logout, done recipes e favorites recipes

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const getUserFromLocalStorage = JSON.parse(localStorage.getItem('user'));
+    const { email } = getUserFromLocalStorage;
+    setUser(email);
+  }, []);
+
   return (
     <div>
       <Header
@@ -10,6 +18,10 @@ function Profile() {
         title="Profile"
       />
       <Header />
+      <p data-testid="profile-email">{user}</p>
+      <button data-testid="profile-done-btn" type="button">Done Recipes</button>
+      <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
+      <button data-testid="profile-logout-btn" type="button">Logout</button>
       <Footer />
     </div>
   );
