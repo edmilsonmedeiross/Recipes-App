7db939b2800771b09d1c3b618a011219abf9931a
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-02-01 18:36:16 -0300

    adiciona redirecionamento de rotas nos botoes e limpa localstorage

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
   const [user, setUser] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     const getUserFromLocalStorage = JSON.parse(localStorage.getItem('user'));
     const { email } = getUserFromLocalStorage;
     setUser(email);
   }, []);
+
+  const handleClickRoute = (route) => {
+    if (route === '/') {
+      localStorage.clear();
+    }
+    history.push(route);
+  };
 
   return (
     <div>
@@ -19,9 +28,27 @@ function Profile() {
       />
       <Header />
       <p data-testid="profile-email">{user}</p>
-      <button data-testid="profile-done-btn" type="button">Done Recipes</button>
-      <button data-testid="profile-favorite-btn" type="button">Favorite Recipes</button>
-      <button data-testid="profile-logout-btn" type="button">Logout</button>
+      <button
+        data-testid="profile-done-btn"
+        type="button"
+        onClick={ () => handleClickRoute('done-recipes') }
+      >
+        Done Recipes
+      </button>
+      <button
+        data-testid="profile-favorite-btn"
+        type="button"
+        onClick={ () => handleClickRoute('/favorite-recipes') }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        data-testid="profile-logout-btn"
+        type="button"
+        onClick={ () => handleClickRoute('/') }
+      >
+        Logout
+      </button>
       <Footer />
     </div>
   );
